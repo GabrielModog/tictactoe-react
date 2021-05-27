a34a17e3cdf4869b3f8425628cb16a6ec8d76016
gabrielmodog <gabrielmodog@gmail.com>
2021-05-27 16:15:55 -0300

    adds components types

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,1 +1,9 @@
+import { ICheckers } from "./checker";
+
+export interface IBoard {
+	status: string | boolean | null;
+	checkers: Array<ICheckers>;
+	whenClick: (currentIndex: number) => void;
+}
+
 export default function Board() {}
--- src/components/checker.tsx
+++ src/components/checker.tsx
@@ -1,1 +1,7 @@
-export default function Checker() {}
+export interface ICheckers {
+	onClick: void;
+	status: boolean | string | null;
+	value: any;
+}
+
+export default function Checker({ value, status, onClick }: ICheckers) {}
