99ad36809363203f89c2e0443f204c54b3d9a1e8
gabrielmodog <gabrielmodog@gmail.com>
2021-05-27 15:15:27 -0300

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	plugins: ["react", "react-native"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:prettier/recommended",
+	],
+	rules: {
+		"react/display-name": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"react/react-in-jsx-scope": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"react/prop-types": "off",
+		"no-extra-boolean-cast": "off",
+		"react-hooks/exhaustive-deps": "off",
+		"no-console": "error",
+		"@typescript-eslint/no-unused-vars": ["off", { ignoreRestSiblings: true }],
+		"@typescript-eslint/no-empty-interface": "off",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
